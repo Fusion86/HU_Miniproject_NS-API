a004286fd4da8b56bcfe0fbfeb742b36e7eeec04
Fusion86 <fusion86@cerbus.nl>
2018-10-10 16:29:00 +0200

    Add Tkinter setup code

--- src/main.py
+++ src/main.py
@@ -1,18 +1,30 @@
 import config
 from nsapi import NSApi
+from tkinter import *
+
+
+class App:
+    def __init__(self, root):
+        # Setup NSApi
+        self.api = NSApi(config.NSAPI_USERNAME, config.NSAPI_PASSWORD)
+
+        # Load stations
+        self.stations = self.api.get_stations()
+        print("Loaded {} stations".format(len(self.stations)))
+
+        # Setup Tkinter
+        self.root = root
+        root.title("NS Miniproject")
+        root.resizable(False, False) # Disable window resizing
 
 
 def main():
-    api = NSApi(config.NSAPI_USERNAME, config.NSAPI_PASSWORD)
+    # Init app
+    root = Tk()
+    app = App(root)
 
-    # Load stations
-    stations = api.get_stations()
-
-    print("Loaded {} stations".format(len(stations)))
-
-    # Test
-    # print(api.actuele_vertrektijden("Utrecht"))
-    # print(api.reisplanner("Utrecht", "Amersfoort"))
+     # Start Tkinter main loop
+    root.mainloop()
 
 
 if __name__ == "__main__":
