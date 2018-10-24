733d460f272f1a4b4d519978694f66d97e64b0d7
Alekos <alekos.kardasis@student.hu.nl>
2018-10-24 13:39:13 +0200

    Redesign main screen

--- src/main.py
+++ src/main.py
@@ -39,25 +39,65 @@ class App:
                font=('Tahoma', 14)).place(x=442, y=545)
 
         Button(frame_start, text='Toon de\nvertrektijden', foreground='white', background='#002A90',
-               font=('Tahoma', 14)).place(x=662, y=545)
+               font=('Tahoma', 14), command=self.show_main).place(x=662, y=545)
 
         img = ImageTk.PhotoImage(Image.open('assets/chipkaart.png'))
         Label(frame_start,image=img).place(x=262, y=240)
 
-
-
-        frame_start.pack()
+        frame_start.grid(row=0, column=0, sticky='nsew')
 
         # Main frame setup
-        frame_main = Frame(root)
+        frame_main = Frame(root, width=1024, height=768)
 
-        frame_main.pack()
+        # Background
+        canvas = Canvas(frame_main, width=1024, height=768, background='#002A90')
+        canvas.pack()
+        canvas.create_rectangle(0,184,1026,688,fill='#FED900', outline='')
+
+        # Header
+        Label(frame_main, text='Vertrekstation',background='#002A90',foreground='white', font=('Tahoma',28)).place(x=20, y=20)
+        Label(frame_main, text='Eindstation',background='#002A90',foreground='white', font=('Tahoma',28)).place(x=20, y=104)
+        Button(frame_main, text='Zoek\ntreinen', background='#5F5FC4', foreground='white', font=('Tahoma',28), command=self.update).place(x=827, y=20)
+
+        # Main content - yellow
+        Label(frame_main, text='Vertrek', background='#FED900', font=('Tahoma',28)).place(x=20, y=204)
+        Label(frame_main, text='Aankomst', background='#FED900', font=('Tahoma', 28)).place(x=173, y=204)
+        Label(frame_main, text='Bestemming', background='#FED900', font=('Tahoma', 28)).place(x=369, y=204)
+        Label(frame_main, text='Type trein', background='#FED900', font=('Tahoma', 28)).place(x=824, y=204)
+
+        # Entries
+        row_height = 50
+        for i in range(self.stations_to_show):
+            # Vetrek
+            lbl = Label(frame_main, text="12:00", background='#FED900', font=('Tahoma',28))
+            lbl.place(x=20, y=271 + row_height * i)
+            setattr(self, "lblVertrekTijd{}".format(i), lbl)
+
+            # Aankomst
+            lbl = Label(frame_main, text="13:00", background='#FED900', font=('Tahoma',28))
+            lbl.place(x=173, y=271 + row_height * i)
+            setattr(self, "lblAankomstTijd{}".format(i), lbl)
+
+            # Bestemming
+            lbl = Label(frame_main, text="Amsterdam Centraal", background='#FED900', font=('Tahoma',28))
+            lbl.place(x=369, y=271 + row_height * i)
+            setattr(self, "lblBestemming{}".format(i), lbl)
+
+            # Type trein
+            lbl = Label(frame_main, text="InterCity", background='#FED900', font=('Tahoma',28))
+            lbl.place(x=824, y=271 + row_height * i)
+            setattr(self, "lblType{}".format(i), lbl)
+
+        # Footer
+        Label(frame_main, text='Terug', background='#002A90', foreground='white', font=('Tahoma', 28)).place(x=81, y=702)
+
+        frame_main.grid(row=0, column=0, sticky='nsew')
 
         self.frame_start = frame_start
         self.frame_main = frame_main
 
         # Show start frame
-        frame_main.tkraise()
+        self.show_start()
 
         # Label(root, text="Begin station").grid(row=0, sticky="w")
         # Label(root, text="Eind station").grid(row=1, sticky="w")
@@ -118,6 +158,12 @@ class App:
 
         if attr:
             attr.config(text=text)
+
+    def show_start(self):
+        self.frame_start.tkraise()
+
+    def show_main(self):
+        self.frame_main.tkraise()
 
 
 def main():
